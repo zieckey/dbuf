f5eb8fe6595fb3d85f077fe935f914e7143a73cc
weizili.laptop <zieckey@gmail.com>
2016-01-28 11:31:31 +0800

    update readme.md

--- README.md
+++ README.md
@@ -3,6 +3,49 @@
 A simple library to manage resources which can be upgraded gracefully. This blog shows how to use it: http://blog.codeg.cn/2016/01/27/double-buffering/
 
 
-During the continuous running period of a network server, there are some resources need to be updated on real-time. How to do an elegant resource upgrading? Here we propose a method to solve this problem which is called "double buffering" technology.
+During the continuous running period of a network server, there are some resources need to be updated on real-time. 
+How to do an elegant resource upgrading? Here we propose a method to solve this problem which is called "double buffering" technology.
 
-When double buffering is enabled, all resource upgrading operations are first initialized to a buffer handler instead of the handler which is being used by the process. After all initializing operations are completed, the buffer handler is swapped directly to the old one associated with it.
+When double buffering is enabled, all resource upgrading operations are first initialized to a buffer handler instead of the handler 
+which is being used by the process. After all initializing operations are completed, the buffer handler is swapped directly to the old one associated with it.
+
+# Example
+
+There is a simple example located in the example directory. The example provides a simple querying service which require tow parameters: id and query.
+`id` is used as an identity of the client. `query` is used as the entity the querying. 
+The service logic uses the `query` as a key to query a backend database and return the result.
+Avoiding vicious client to do request we provide an black-id-list which holds all the black ids.
+
+Using the following steps to run this example:
+
+### Step 1
+
+```shell
+$ git clone https://github.com/zieckey/go-doublebuffering
+$ cd go-doublebuffering/example
+$ go build
+$ ./example ./black_id.txt
+```
+
+### Step 2
+
+And in another console, we can do some requests:
+
+```shell
+$ curl "http://localhost:8091/q?id=123&query=jane"
+hello, 123
+$ curl "http://localhost:8091/q?id=475e5a499587a52ea14a23031ecce7c9&query=jane"
+ERROR
+```
+
+### Step 3
+
+If we have updated the black-id-list file, we just need to send a administration request to have it enabled. Like this:
+
+```shell
+$ curl "http://localhost:8091/admin/reload?name=black_id&path=./black_id.txt"
+OK
+```
+
+
+
--- example/main.go
+++ example/main.go
@@ -1,4 +1,4 @@
-package dbuf
+package main
 
 import (
 	"net/http"
@@ -29,7 +29,7 @@ var dbm *dbuf.DoubleBufferingManager
 func (d *BlackIDDict) Initialize(conf string) bool {
 	filepath := conf
 
-	// 加载黑名单列表文件，每行一个
+	// A black name list
 	b, err := ioutil.ReadFile(filepath)
 	if err != nil {
 		return false
@@ -53,8 +53,7 @@ func (d *BlackIDDict) Initialize(conf string) bool {
 }
 
 func (d *BlackIDDict) Close() {
-	// 在这里做一些资源释放工作
-	// 当前这个例子没有资源需要我们手工释放
+	// We can do some resource recycling works here if necessarily
 }
 
 func (d *BlackIDDict) IsBlackID(id string) bool {
@@ -66,12 +65,14 @@ func Query(r *http.Request) (string, error) {
 	id := r.FormValue("id")
 	query := r.FormValue("query")
 
-	//TODO 参数合法性检查
+	if len(id) == 0 || len(query) == 0 {
+		return "", fmt.Errorf("Request parameter error")
+	}
 
 	d := dbm.Get("black_id")
 	tg := d.Get()
 	defer tg.Release()
-	dict := tg.Target.(*BlackIDDict)  // 转换为具体的Dict对象
+	dict := tg.Target.(*BlackIDDict)  // convert to concrete object of BlackIDDict
 	if dict == nil {
 		return "", fmt.Errorf("ERROR, Convert DoubleBufferingTarget to Dict failed")
 	}
@@ -80,11 +81,10 @@ func Query(r *http.Request) (string, error) {
 		return "ERROR", fmt.Errorf("ERROR id")
 	}
 
-	//具体的业务逻辑，查询数据库/NoSQL等数据引擎，然后做逻辑计算，然后合并结果
-	//这里简单抽象，直接返回欢迎语
+	// This is the application business logic, something like query MySQL, calculate, merge result.
 	result := fmt.Sprintf("hello, %v", id)
 
-	// 记录一条查询日志，用于离线统计和分析
+	// Record a query log
 	log.Printf("<id=%v><query=%v><result=%v><ip=%v>", id, query, result, r.RemoteAddr)
 
 	return result, nil
@@ -102,8 +102,11 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func Reload(w http.ResponseWriter, r *http.Request) {
-	// 这里简化处理，直接重新加载black_id。如果有多个，可以从url参数中获取资源名称、资源所在的路径
-	if dbm.Reload("black_id", os.Args[1]) {
+	r.ParseForm()
+	name := r.FormValue("name")
+	path := r.FormValue("path")
+
+	if dbm.Reload(name, path) {
 		w.Write([]byte("OK"))
 	} else {
 		w.Write([]byte("FAILED"))
@@ -121,8 +124,8 @@ func main() {
 		panic("black_id initialize failed")
 	}
 
-	http.HandleFunc("/q", Handler)
-	http.HandleFunc("/admin/reload", Reload) // 管理接口，用于重新加载black_id.txt。如果有多个这种资源，可以增加一些参数来说区分不同的资源
+	http.HandleFunc("/q", Handler)  // query interface
+	http.HandleFunc("/admin/reload", Reload) // admin interface
 	hostname, _ := os.Hostname()
 	log.Printf("start http://%s:8091/q", hostname)
 	log.Fatal(http.ListenAndServe(":8091", nil))
